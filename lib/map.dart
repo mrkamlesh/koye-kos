323779bd0275d47c532ad318b77596c315c60f25
Øyvind Samuelsen <oyvinsam@stud.ntnu.no>
2020-07-09 10:18:29 +0200

    add a marker for a camp location

--- lib/map.dart
+++ lib/map.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong/latlong.dart';
 
@@ -7,16 +8,35 @@ class MapInfo {
       'gatekeeper/gk/gk.open_gmaps?'
       'layers=topo4&zoom={z}&x={x}&y={y}&format=image/jpeg';
   static final mapSubdomains = ['', '2', '3'];
+  static final LatLng defaultLatLng = LatLng(59.81, 10.44);
 }
 
 
 class HammockMap extends StatelessWidget {
 
+  Image image = Image.asset(
+    'spot_1.jpg',
+    width: 20,
+    height: 20,
+    fit: BoxFit.cover,
+  );
+  static final LatLng campPoint = LatLng(59.813833, 10.412977);
+
+  List<Marker> markers = <Marker> [
+    Marker(
+      point: campPoint,
+      width: 40,
+      height: 40,
+      anchorPos: AnchorPos.align(AnchorAlign.top),
+      builder: (context) => Icon(Icons.location_on, size: 40),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return FlutterMap(
       options: MapOptions(
-        center: LatLng(59.81, 10.44),
+        center: MapInfo.defaultLatLng,
         zoom: 12.0,
       ),
       layers: [
@@ -24,19 +44,10 @@ class HammockMap extends StatelessWidget {
             urlTemplate: MapInfo.mapUrl,
             subdomains: MapInfo.mapSubdomains),  // loadbalancing; uses subdomains opencache[2/3].statkart.no
         MarkerLayerOptions(
-          markers: [
-            Marker(
-              width: 40.0,
-              height: 40.0,
-              point: LatLng(59.81, 10.44),
-              builder: (ctx) =>
-                  Container(
-                      child: FlutterLogo(),
-                  ),
-            ),
-          ],
+          markers: markers,
         ),
       ],
     );
   }
 }
+
