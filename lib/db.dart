2e02235bd57cff22981a109c82d436dfd0d20972
Øyvind Samuelsen <oyvinsam@stud.ntnu.no>
2020-07-15 17:09:41 +0200

    Only query data one time for reduced data transfer

--- lib/db.dart
+++ lib/db.dart
@@ -1,11 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-import 'data.dart';
-
 class DatabaseService {
   final Firestore _db = Firestore.instance;
 
   Stream<QuerySnapshot> getCampSnapshot() {
-    return _db.collection('camps').snapshots();
+    // use ('camps').snapshots for continuous connection with live updates
+    return _db.collection('camps').getDocuments().asStream();
   }
 }
