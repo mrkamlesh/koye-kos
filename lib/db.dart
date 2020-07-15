227a4fbeccb3c0bd1cae69b95e73d9ba21a4a4db
Øyvind Samuelsen <oyvinsam@stud.ntnu.no>
2020-07-15 19:34:26 +0200

    Add function to get stream of camps

--- lib/db.dart
+++ lib/db.dart
@@ -9,11 +9,17 @@ class FirestoreService {
 
   Future<List<CampMarker>> getCampMarkerFuture() {
     // use ('camps').snapshots for continuous connection with live updates
-    return Firestore.instance.collection('camps').getDocuments()
-    .then((QuerySnapshot query) => query.documents
-        .map((DocumentSnapshot document) => Camp.fromFirestore(document))
-        .map((Camp camp) => CampMarker(camp))
-        .toList()
-    );
+    return Firestore.instance.collection('camps').getDocuments().then(
+        (QuerySnapshot query) => query.documents
+            .map((DocumentSnapshot document) => Camp.fromFirestore(document))
+            .map((Camp camp) => CampMarker(camp))
+            .toList());
+  }
+
+  Stream<List<Camp>> getCampStream() {
+    // use ('camps').snapshots for continuous connection with live updates
+    return Firestore.instance.collection('camps').snapshots().map(
+        (QuerySnapshot snapshot) => snapshot.documents
+            .map((DocumentSnapshot document) => Camp.fromFirestore(document)));
   }
 }
