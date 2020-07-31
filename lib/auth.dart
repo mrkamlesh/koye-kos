d2f75477bc7708e0626461436119c26e5aa32957
Øyvind Samuelsen <oyvinsam@stud.ntnu.no>
2020-07-31 09:05:53 +0200

    Update user detail when linking since it is not updated automatically

--- lib/auth.dart
+++ lib/auth.dart
@@ -5,27 +5,6 @@ import 'package:provider/provider.dart';
 
 import 'db.dart';
 
-class SignInWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final user = Provider.of<FirebaseUser>(context);
-    final firestoreService = Provider.of<FirestoreService>(context);
-    print('is anonomous: ${user?.isAnonymous}');
-    print('name ${user?.displayName}');
-    final _signedIn = user != null ? !user.isAnonymous : false;
-    print(_signedIn);
-    return FlatButton(
-      child: _signedIn ? Text('Log out') : Text('Log in with Google'),
-      onPressed: () {
-        return _signedIn
-            ? AuthService.signOut()
-            : AuthService.signInWithGoogle();
-      },
-      textColor: Theme.of(context).buttonColor,
-    );
-  }
-}
-
 class AuthService {
   AuthService._();
   static final instance = AuthService._();
@@ -33,33 +12,42 @@ class AuthService {
   static final FirebaseAuth _auth = FirebaseAuth.instance;
   static final GoogleSignIn _googleSignIn = GoogleSignIn();
 
-  static void signOut() async {
-    await _auth.currentUser().then((value) {
-      value.unlinkFromProvider(value.providerId);
-    });
-    /*await _auth.signOut();
-    await _auth.signInAnonymously();*/
+  Future<FirebaseUser> get currentUser => _auth.currentUser();
+
+  void signOut() async {
+    _googleSignIn.signOut();
+    _auth.signOut();
+/*    FirebaseAuth.instance.currentUser()
+        .then((FirebaseUser user) {
+      user.unlinkFromProvider(user.providerId)
+    })
+        .catchError((onError) => print('ERROR UNLINKING: $onError'));*/
   }
 
-  static void signInAnonymously() async {
+  void signInAnonymously() async {
     await _auth.signInAnonymously();
   }
 
-  static void signInWithGoogle() async {
+  Future<bool> signInWithGoogle() async {
     final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
-    final GoogleSignInAuthentication googleAuth =
-        await googleUser.authentication;
+    final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
     final AuthCredential credential = GoogleAuthProvider.getCredential(
       accessToken: googleAuth.accessToken,
       idToken: googleAuth.idToken,
     );
+
     _auth.currentUser().then((FirebaseUser user) {
       user.linkWithCredential(credential).then((AuthResult result) {
         print('linked user');
-        print(result.user.displayName);
-        //assert(result.user.displayName != null); // simple check sign in works
+        UserUpdateInfo userInfo = UserUpdateInfo();
+        userInfo
+          ..displayName = googleUser.displayName
+          ..photoUrl = googleUser.photoUrl;
+        result.user.updateProfile(userInfo);
+        _auth.currentUser().then((value) => print('new din: ${value.displayName}'));
+      })
+          .catchError((error) {
+        print('ERROR LINKING: $error');
       });
-      print(user);
-    });
-  }
+  });
 }
