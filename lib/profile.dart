750ff6796a6c2d938d868285dd7b4071d8daa454
Øyvind Samuelsen <oyvinsam@stud.ntnu.no>
2020-07-31 16:42:46 +0200

    Added favorited/rated/created list elements to user profile

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:koye_kos/auth.dart';
 import 'package:koye_kos/main.dart';
 import 'package:provider/provider.dart';
@@ -9,16 +10,15 @@ class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final FirebaseUser user = Provider.of<FirebaseUser>(context);
-    AuthService.instance.currentUser.then((value) => print('Displayname:: ${value?.displayName}'));
+    AuthService.instance.currentUser
+        .then((value) => print('Displayname:: ${value?.displayName}'));
     /*print(user);
     print(user?.uid);
     print(user?.isAnonymous);
     print(user?.email);
     print(user?.displayName);
     print(user == null || user.isAnonymous);*/
-    return user == null || user.isAnonymous
-            ? SignUpView()
-            : AccountView();
+    return user == null || user.isAnonymous ? SignUpView() : AccountView();
   }
 }
 
@@ -82,17 +82,52 @@ class AccountView extends StatelessWidget {
       ),
       body: Center(
         child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 8.0,),
+          padding: const EdgeInsets.symmetric(
+            vertical: 8,
+            horizontal: 8,
+          ),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Padding(
                 padding: const EdgeInsets.symmetric(vertical: 16),
-                child: ClipOval(child: CachedNetworkImage(imageUrl: user.photoUrl,),),
+                child: ClipOval(
+                  child: CachedNetworkImage(
+                    imageUrl: user.photoUrl,
+                  ),
+                ),
               ),
-              Text('${user.displayName}', style: Theme.of(context).textTheme.headline6,),
-              Text('${user.email}', style: Theme.of(context).textTheme.headline6),
-              SizedBox(height: 50,),
+              Text(
+                '${user.displayName}',
+                style: Theme.of(context).textTheme.headline6,
+              ),
+              Text('${user.email}',
+                  style: Theme.of(context).textTheme.headline6),
+              Divider(height: 40),
+              ListTile(
+                leading: Icon(
+                  Icons.favorite,
+                  color: Colors.red,
+                ),
+                title: Text('Favorited camps'),
+                onTap: () => print('opencontainer todo'),
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.star,
+                  color: Colors.amber,
+                ),
+                title: Text('Rated camps'),
+                onTap: () => print('opencontainer todo'),
+              ),
+              ListTile(
+                leading: FaIcon(
+                  FontAwesomeIcons.mapMarkedAlt,
+                  color: Colors.green,
+                ),
+                title: Text('Created camps'),
+                onTap: () => print('opencontainer todo'),
+              ),
               Expanded(
                 child: Align(
                   alignment: FractionalOffset.bottomCenter,
