82a5c51bcab118167f69e965e3976aee040e7589
Øyvind Samuelsen <oyvinsam@stud.ntnu.no>
2020-08-30 13:00:41 +0200

    Add dialog for showing user they need to log in to use x feature

---
+++ lib/ui/dialog.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class LogInDialog extends StatelessWidget {
+  final String actionText;
+
+  LogInDialog({@required this.actionText})
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text('Log in'),
+      content: SingleChildScrollView(
+        child: ListBody(
+          children: <Widget>[
+            Text(
+                'You need to be logged in to $actionText.'),
+            Text('Log in or register a user now.'),
+          ],
+        ),
+      ),
+      actions: <Widget>[
+        FlatButton(
+          child: Text('Continue without user'),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        FlatButton(
+          child: Text('Log in / register'),
+          onPressed: () {
+            Navigator.of(context).pop();
+            Navigator.of(context).pushNamed('/profile');
+          },
+        ),
+      ],
+    );
+  }
+}
