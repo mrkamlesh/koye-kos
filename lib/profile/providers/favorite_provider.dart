793eb5f47fc6521ea3ce0a9bbb45b237bfb498ec
Øyvind Samuelsen <oyvinsam@stud.ntnu.no>
2020-09-01 18:46:03 +0200

    Sort favorited list from recent to old favorite time

--- lib/profile/providers/favorite_provider.dart
+++ lib/profile/providers/favorite_provider.dart
@@ -9,10 +9,14 @@ class FavoriteModel with ChangeNotifier {
   FirestoreService firestore;
   StreamSubscription<List<Favorite>> _favoriteCampIdsSub;
   List<String> _favoriteCampIds;
-  String _unfavoritedCampId;
+  // Map camp id to 'position' in list, where a position 0 is the most recent favorited id. 
+  // Used to sort (a general) camp stream based on user favorited data. 
+  Map<String, int> _campIdToFavoritedPosition;
+  String _unfavoritedCampIdTemp;  // temporarily store the most recent unfavorited camp id
 
   FavoriteModel({@required this.firestore}) {
-    _favoriteCampIdsSub = firestore.campIdsFavoritedStream().listen(_onFavoriteCampIds);
+    _favoriteCampIdsSub =
+        firestore.campIdsFavoritedStream().listen(_onFavoriteCampIds);
   }
 
   void setFirestore(FirestoreService firestore) => this.firestore = firestore;
@@ -20,18 +24,27 @@ class FavoriteModel with ChangeNotifier {
   // Favorite
   void _onFavoriteCampIds(List<Favorite> campIds) async {
     _favoriteCampIds = campIds.map((e) => e.campId).toList();
+    _campIdToFavoritedPosition =
+        _favoriteCampIds.asMap().map((position, id) => MapEntry(id, position));
   }
 
-  Stream<List<Favorite>> get favoriteCampIdsStream => firestore.campIdsFavoritedStream();
-  Stream<List<Camp>> get favoriteCampsStream => firestore.getCampsStream(_favoriteCampIds);
+  Stream<List<Favorite>> get favoriteCampIdsStream =>
+      firestore.campIdsFavoritedStream();
+  // Sort based on favorited time
+  Stream<List<Camp>> get favoriteCampsStream {
+    return firestore.getCampsStream(_favoriteCampIds).map((event) {
+      event.sort((a, b) => _campIdToFavoritedPosition[a.id] - _campIdToFavoritedPosition[b.id]);
+      return event;
+    });
+  }
 
   void unfavorite(String campId) {
     firestore.setFavorited(campId, favorited: false);
-    _unfavoritedCampId = campId;
+    _unfavoritedCampIdTemp = campId;
   }
 
   void undoFavorite() {
-    firestore.setFavorited(_unfavoritedCampId);
+    firestore.setFavorited(_unfavoritedCampIdTemp);
   }
 
   @override
