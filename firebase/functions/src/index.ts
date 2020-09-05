14787477d184b2cda25268a1f12de07e9e0c8d7c
Øyvind Samuelsen <oyvinsam@stud.ntnu.no>
2020-09-05 15:45:38 +0200

    Add reaction (like/dislike) cloud functions

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -2,7 +2,7 @@ import * as admin from "firebase-admin";
 
 const functions = require('firebase-functions');
 
-import {EventContext} from "firebase-functions";
+import {Change, EventContext} from "firebase-functions";
 import { QueryDocumentSnapshot } from "firebase-functions/lib/providers/firestore";
 import DocumentSnapshot = admin.firestore.DocumentSnapshot;
 
@@ -34,5 +34,36 @@ exports.onCommentReportDelete = functions.firestore
             if (!commentDoc.exists) return;
             const increment = admin.firestore.FieldValue.increment(-1);
             return commentRef.update({'reports': increment});
+        });
+    });
+
+exports.onReaction = functions.firestore
+    .document('camps/{campId}/comments/{commentId}/reactions/{reactionId}')
+    .onWrite((change: Change<DocumentSnapshot>, context:EventContext) => {
+        if (change.before === change.after) return;  // no change
+        const campId: String = context.params.campId;
+        const commentId: String = context.params.commentId;
+        const reactionId: String = context.params.reactionId;
+        const commentRef = db.doc(`camps/${campId}/comments/${commentId}`);
+        const reactionRef = db.doc(`camps/${campId}/comments/${commentId}/reactions/${reactionId}`);
+        return commentRef.get().then((commentDoc: DocumentSnapshot) => {
+            if (!commentDoc.exists) return;
+            reactionRef.get().then((reactionDoc: DocumentSnapshot) => {
+                // deleted or changed -> remove old reaction
+                if (!reactionDoc.exists || change.before.exists) {
+                    const likedBefore = <boolean>change.before.get('liked');
+                    const revertIncrement = admin.firestore.FieldValue.increment(-1);
+                    likedBefore
+                        ? commentRef.update({likes: revertIncrement})
+                        : commentRef.update({dislikes: revertIncrement});
+                    if (!reactionDoc.exists) return;  // reaction deleted -> return
+                }
+                // created/changed -> add reaction
+                const liked = <boolean>change.after.get('liked');
+                const increment = admin.firestore.FieldValue.increment(1);
+                return liked
+                    ? commentRef.update({likes: increment})
+                    : commentRef.update({dislikes: increment})
+            });
         });
     });
