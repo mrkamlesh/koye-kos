ff8c7ce55dc4a67690c1ba31ec6005cf33e2313c
Øyvind Samuelsen <oyvinsam@stud.ntnu.no>
2020-09-05 13:37:02 +0200

    Update report count on report added

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,9 +1,25 @@
-import * as functions from 'firebase-functions';
+import * as admin from "firebase-admin";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const functions = require('firebase-functions');
+
+import {EventContext} from "firebase-functions";
+import { QueryDocumentSnapshot } from "firebase-functions/lib/providers/firestore";
+import DocumentSnapshot = admin.firestore.DocumentSnapshot;
+
+admin.initializeApp();
+
+const db = admin.firestore();
+
+exports.onCommentReport = functions.firestore
+    .document('camps/{campId}/comments/{commentId}/reports/{reportId}')
+    .onCreate((snapshot: QueryDocumentSnapshot, context:EventContext) => {
+        const campId: String = context.params.campId;
+        const commentId: String = context.params.commentId;
+        const commentRef = db.doc(`camps/${campId}/comments/${commentId}`);
+        return commentRef.get().then((commentDoc: DocumentSnapshot) => {
+            if (!commentDoc.exists) return;
+            const increment = admin.firestore.FieldValue.increment(1);
+            return commentRef.update({'reports': increment});
+        });
+
+    });
