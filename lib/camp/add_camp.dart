95b367b003f5a8217cdef4e6233c753b572a63a4
Øyvind Samuelsen <oyvinsam@stud.ntnu.no>
2020-09-12 12:15:03 +0200

    Re-add add camp files. For web support need to use non native image picker and editing library

---
+++ lib/camp/add_camp.dart
@@ -0,0 +1,358 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:implicitly_animated_reorderable_list/implicitly_animated_reorderable_list.dart';
+import 'package:koye_kos/camp/ui/feature_chips.dart';
+import 'package:koye_kos/models/camp.dart';
+import 'package:provider/provider.dart';
+import 'package:transparent_image/transparent_image.dart';
+
+import 'providers/add_camp_model.dart';
+
+class AddCampScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final addModel = Provider.of<AddModel>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Add camp (${addModel.readableLocation})',
+        ),
+        actions: [
+          AddCampButton(),
+        ],
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          AddImageView(),
+          FeatureSelectionWidget(),
+          CampForm(),
+        ],
+      ),
+    );
+  }
+}
+
+class FeatureSelectionWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final addModel = Provider.of<AddModel>(context);
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 16, left: 8, right: 8),
+      child: Wrap(
+        spacing: 8,
+        runSpacing: 8,
+        crossAxisAlignment: WrapCrossAlignment.start,
+        children: [
+          FeatureSelectChip(
+            title: 'Tent',
+            feature: CampFeature.Tent,
+            isSelected: addModel.tentSelected,
+            onSelected: addModel.onFeatureSelected,
+          ),
+          FeatureSelectChip(
+            title: 'Hammock',
+            feature: CampFeature.Hammock,
+            isSelected: addModel.hammockSelected,
+            onSelected: addModel.onFeatureSelected,
+          ),
+          FeatureSelectChip(
+            title: 'Water nearby',
+            feature: CampFeature.Water,
+            isSelected: addModel.waterSelected,
+            onSelected: addModel.onFeatureSelected,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class AddCampButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final addModel = Provider.of<AddModel>(context);
+    return FlatButton(
+      child: Text(
+        'POST',
+        style: TextStyle(
+            color: addModel.canPost
+                ? Colors.white
+                : Colors.white.withOpacity(0.8)),
+      ),
+      onPressed: () {
+        bool wasAdded = addModel.postPressed();
+        wasAdded
+            ? Navigator.pop(context, true)
+            : Scaffold.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(
+                    'Error uploading camp!',
+                  ),
+                ),
+              );
+      },
+    );
+  }
+}
+
+class AddImageView extends StatefulWidget {
+  @override
+  _AddImageViewState createState() => _AddImageViewState();
+}
+
+class _AddImageViewState extends State<AddImageView> {
+  final _listKey = GlobalKey<AnimatedListState>();
+
+  Future getImage(ImageSource source) async {
+    final addModel = context.read<AddModel>();
+    ImagePicker()
+        .getImage(source: source)
+        .then((PickedFile pickedFile) {
+      if (pickedFile == null) return;
+      final sourcePath = pickedFile.path;
+      cropImage(sourcePath).then((File fileImage) {
+        if (fileImage == null) return;
+        addModel.addImage(sourcePath: sourcePath, croppedFile: fileImage);
+      });
+    }).catchError((error) {
+      Scaffold.of(context)
+        ..removeCurrentSnackBar()
+        ..showSnackBar(SnackBar(
+          content: Text('Could not add image: $error'),
+        ));
+    });
+  }
+
+  Future<File> cropImage(String path) {
+    return ImageCropper.cropImage(
+      sourcePath: path,
+      compressFormat: ImageCompressFormat.jpg,
+      // default
+      compressQuality: 100,
+      // default 90, do compression on image upload
+      aspectRatio: CropAspectRatio(ratioX: 4, ratioY: 3),
+      androidUiSettings: AndroidUiSettings(
+        toolbarTitle: 'Crop image',
+        toolbarColor: Theme.of(context).primaryColor,
+        toolbarWidgetColor: Colors.white,
+        lockAspectRatio: true,
+        hideBottomControls: true,
+      ),
+      iosUiSettings: IOSUiSettings(
+        aspectRatioPickerButtonHidden: true,
+        aspectRatioLockEnabled: true,
+        title: 'Crop image',
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final addModel = Provider.of<AddModel>(context);
+
+    return Container(
+      width: double.infinity,
+      child: Column(
+        children: [
+          SizedBox(height: 8),
+          ImageList(
+            listKey: _listKey,
+            addCallback: getImage,
+            onEditCallback: (int index) {
+              cropImage(addModel.getSourceImage(index).path)
+                  .then((file) => addModel.updateImage(index, file));
+            },
+          ),
+          addModel.showNoImageError
+              ? Container(
+                  height: 24,
+                  child: Center(
+                    child: Text(
+                      'Please add at least 1 image!',
+                      style: TextStyle(
+                        color: Colors.red.shade700,
+                        fontSize: 12,
+                      ),
+                    ),
+                  ),
+                )
+              : SizedBox(height: 24), // Empty space so error doesn't 'jump' in
+        ],
+      ),
+    );
+  }
+}
+
+class ImageList extends StatelessWidget {
+  final GlobalKey<AnimatedListState> listKey;
+  final Function(ImageSource) addCallback;
+  final Function(int) onEditCallback;
+  static const double imageHeight = 210;
+  static const double imageWidth = imageHeight * 4 / 3;
+
+  ImageList(
+      {@required this.listKey,
+      @required this.addCallback,
+      @required this.onEditCallback});
+
+  @override
+  Widget build(BuildContext context) {
+    final addModel = Provider.of<AddModel>(context);
+    return Container(
+      height: imageHeight,
+      child: ImplicitlyAnimatedReorderableList<CampImage>(
+        key: listKey,
+        items: addModel.campImages,
+        scrollDirection: Axis.horizontal,
+        shrinkWrap: true,
+        areItemsTheSame: (a, b) => a.sourceFile.path == b.sourceFile.path,
+        onReorderFinished: addModel.onReorderFinished,
+        itemBuilder: (context, itemAnimation, item, index) {
+          return Reorderable(
+              key: ValueKey(item),
+              builder: (context, dragAnimation, inDrag) {
+                return SizeTransition(
+                  axis: Axis.horizontal,
+                  sizeFactor: itemAnimation,
+                  child: Handle(
+                    delay: const Duration(milliseconds: 500),
+                    child: Container(
+                      width: imageWidth,
+                      padding: EdgeInsets.only(right: 2),
+                      child: Stack(
+                        fit: StackFit.expand,
+                        children: [
+                          CampImageWidget(
+                            campImage: item,
+                          ),
+                          Positioned(
+                            left: 0,
+                            top: 0,
+                            child: IconButton(
+                                icon: Icon(
+                                  Icons.crop,
+                                  color: Colors.white,
+                                ),
+                                onPressed: () {
+                                  onEditCallback(index);
+                                }),
+                          ),
+                          Positioned(
+                            right: 2,
+                            top: 2,
+                            child: IconButton(
+                              icon: Icon(
+                                Icons.close,
+                                color: Colors.white,
+                              ),
+                              onPressed: () => addModel.removeImage(index),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              });
+        },
+        footer: Container(
+          width: imageWidth,
+          padding: EdgeInsets.all(1),
+          decoration: BoxDecoration(
+              border: Border.all(
+                  width: 1,
+                  color: addModel.showNoImageError
+                      ? Colors.red.shade700
+                      : Colors.blue),
+              borderRadius: BorderRadius.all(Radius.circular(1))),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: FlatButton(
+                  child: Icon(Icons.add_a_photo),
+                  onPressed: () => addCallback(ImageSource.camera),
+                ),
+              ),
+              Container(
+                  margin: EdgeInsets.symmetric(vertical: 10),
+                  child: VerticalDivider(width: 2)),
+              Expanded(
+                child: FlatButton(
+                  child: Icon(Icons.add_photo_alternate),
+                  onPressed: () => addCallback(ImageSource.gallery),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CampImageWidget extends StatefulWidget {
+  final CampImage campImage;
+  CampImageWidget({this.campImage, Key key}) : super(key: key);
+
+  @override
+  _CampImageWidgetState createState() => _CampImageWidgetState();
+}
+
+class _CampImageWidgetState extends State<CampImageWidget>
+    with AutomaticKeepAliveClientMixin {
+  @override
+  bool get wantKeepAlive => true;
+
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+    if (widget.campImage.isLoading) {
+      return Center(
+        child: CircularProgressIndicator(),
+      );
+    } else {
+      return FadeInImage(
+        image: widget.campImage.fileImage,
+        placeholder: MemoryImage(kTransparentImage),
+        fit: BoxFit.cover,
+      );
+    }
+  }
+}
+
+class CampForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final addModel = Provider.of<AddModel>(context);
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: Form(
+        child: TextFormField(
+          keyboardType: TextInputType.multiline,
+          minLines: 1,
+          maxLines: 5,
+          decoration: InputDecoration(
+              hintText: 'Enter a short camp description',
+              labelText: 'Description',
+              errorStyle: TextStyle(
+                color: Colors.red.shade700,
+                fontSize: 12,
+              ),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
+                borderSide: BorderSide(),
+              )),
+          validator: (_) => addModel.descriptionValidator,
+          onChanged: (value) => addModel.onDescriptionChanged(value),
+          autovalidate: addModel.autoValidate,
+        ),
+      ),
+    );
+  }
+}
